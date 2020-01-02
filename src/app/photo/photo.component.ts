339afba0205d19195d8652602ca95abca6c45297
maycon.rosario <maycon.rosario@spassu.com>
2020-01-02 18:11:14 -0300

    05 Inbound properties: passando dados para o componente

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'ap-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent implements OnInit {
+
+  @Input() url = '';
+  @Input() description = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
